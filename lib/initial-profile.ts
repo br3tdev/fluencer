85f99ff70d8583e6b621abc7b59680a2133b478f
bret <bretai@LCARS-MX-23>
2024-01-23 22:53:08 +0300

    fixed getInitialProfile()

--- lib/initial-profile.ts
+++ lib/initial-profile.ts
@@ -1,9 +1,10 @@
-import { currentUser, redirectToSignIn } from "@clerk/nextjs";
+import { auth, currentUser, redirectToSignIn } from "@clerk/nextjs";
 
 import { db } from "@/lib/db";
 
 export async function getInitialProfile() {
   const user = await currentUser();
+  const { orgId } = auth();
 
   if (!user) return redirectToSignIn();
 
@@ -20,6 +21,7 @@ export async function getInitialProfile() {
       userId: user.id,
       name: `${user.firstName} ${user.lastName}`,
       email: user.emailAddresses[0].emailAddress,
+      orgId: orgId as string,
     },
   });
 
